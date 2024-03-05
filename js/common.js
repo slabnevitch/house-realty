6e3634f6c88a6f2c70615e74ab98ba3305ceea50
Slabnevitch <handofschpilrain@mail.ru>
2024-03-05 23:20:24 +0500

    отправка главной стр. на проверку

--- js/common.js
+++ js/common.js
@@ -66,7 +66,78 @@
 
 // });
 
+(function() {
+	// ibg class
+	if('objectFit' in document.documentElement.style === false){
+	  Array.prototype.forEach.call(document.querySelectorAll('._fit'), function(el){
+
+	    var image = el.querySelector('img');
+	    el.style.backgroundImage = 'url("'+image.src+'")';
+	    el.classList.add('ibg');
+	    el.classList.remove('_fit');
+		 });
+	}
+	// End ibg class
+
+	document.addEventListener('DOMContentLoaded', function() {
+		console.log('DOMContentLoaded!');
+		// password show
+		if(document.querySelector('.eye-ico') !== null){
+			[].forEach.call(document.querySelectorAll('.eye-ico'), function(el) {
+				el.onclick = function(e) {
+					var passInput = this.closest('label').querySelector('.form__input');
+					this.classList.toggle('unblind');
+					passInput.getAttribute('type') === 'password' ?
+					passInput.setAttribute('type','text') :
+					passInput.setAttribute('type','password');
+				}
+			});
+		}
+		// END password show
 
 
-// запуск основной функции
-// einput.init();
+		// enter-form toggle
+		if(document.querySelector('.page-main__form-display') !== null){
+			document.querySelector('.page-main__form-display .btn').onclick = function() {
+				this.closest('.page-main__forms').classList.add('active');
+			}
+		}
+		// END enter-form toggle
+		
+		// micromodal
+		if(document.querySelector('.modal') !== null){
+			MicroModal.init({
+				openTrigger: 'data-micromodal-open', 
+				closeTrigger: 'data-micromodal-close',
+				openClass: 'is-open', 
+				disableFocus: true, 
+				awaitOpenAnimation: true,
+				awaitCloseAnimation: true,
+				// disableScroll: true,
+				onShow: function(modal, trigger, event){
+
+					// console.log(trigger)
+					// console.log(event)
+					// console.log(modal)	
+					
+					// при disableScroll: true для компенсации ширины скроллбара (фикс "прыгания" страницы влево)
+					if(document.querySelector('#wrapper-for-scroll-fix') != null){
+						document.querySelector('#wrapper-for-scroll-fix').classList.add('modal-open');
+
+					}
+
+				},
+				onClose: function(modal) {
+					// console.info(`${modal.id} is hidden`);
+					// при disableScroll: true для компенсации ширины скроллбара (фикс "прыгания" страницы влево)
+					if(document.querySelector('#wrapper-for-scroll-fix') != null){
+						document.querySelector('#wrapper-for-scroll-fix').classList.remove('modal-open');
+
+					}
+
+				}
+			});		
+		}
+	// END micromodal
+	});
+})();
